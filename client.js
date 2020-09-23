47c6992a4b017ed6abb02ca939960b6574634e5c
kieranSharley <ksharley9@gmail.com>
2020-09-23 19:46:22 +0000

    now i have a name

--- client.js
+++ client.js
@@ -7,14 +7,19 @@ const connect = function() {
   const conn = net.createConnection({ 
     host: '135.23.222.131',
     port: 50542
-  });
+  });  
+  // interpret incoming data as text
+  conn.setEncoding('utf8'); 
+
   //Update the connect function to also attach an event handler to handle incoming data and console log it for the player.
   conn.on (`data`, (data) => {
     console.log(`Incoming:`, data);
   })  
+  conn.on(`connect`, () => {
+    console.log('Successfully connected to game server');
+    conn.write("Name: ABC")
+  });
   
-  // interpret incoming data as text
-  conn.setEncoding('utf8'); 
 
   return conn;
 }
